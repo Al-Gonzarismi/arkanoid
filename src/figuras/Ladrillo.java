76a94e4eabe72aa4c6b1ac7f90f6ae65ceb89cc9
gonza <1_web9_20@iesjovellanos.org>
2019-04-01 14:18:10 +0200

    FALLOS: Pelota se mueve al quitar vida, reiniciar nivel bien, rebote al chocar en dos ladrillos

--- src/figuras/Ladrillo.java
+++ src/figuras/Ladrillo.java
@@ -8,6 +8,8 @@ package figuras;
 import figuras.base.Dibujable;
 import figuras.base.Eliminable;
 import figuras.base.Sprite;
+import java.awt.Color;
+import java.awt.Graphics;
 import java.awt.Rectangle;
 import logic.*;
 import java.util.*;
@@ -17,8 +19,12 @@ import java.util.*;
  */
 public class Ladrillo extends Sprite implements Eliminable {
     Rectangle ladrillo;
+    Rectangle ladrilloLadoIzq;
+    Rectangle ladrilloLadoDcha;
     Rectangle pelotita;
     GameLogic logic;
+    int numeroSkin;
+    int golpes;
 
     public Ladrillo(GameLogic logica, int x, int y, int numeroSkin) {
         super(new String[]{
@@ -33,15 +39,23 @@ public class Ladrillo extends Sprite implements Eliminable {
         });
         setX(x);
         setY(y);
+        if (numeroSkin == 7) {
+            this.numeroSkin = 7;
+            golpes = 0;
+        }
         setSkin(numeroSkin);
         setWidth(60);
-        this.logic = logica;       
+        setHeight(22);
+        this.logic = logica;
     }
-
+    
+    
+    
     @Override
     public boolean estaEliminado() {
-        
         ladrillo = new Rectangle(getX(), getY(), getWidth(), getHeight());
+        ladrilloLadoIzq = new Rectangle(getX(), getY() + 2, 1, getHeight() - 4);
+        ladrilloLadoDcha = new Rectangle(getX() + getWidth(), getY() + 2, 1, getHeight() - 4);
         Pelota p = null;
         List<Dibujable> l = new LinkedList<>(logic.getListaObjetos());
         for (Dibujable d : l) {
@@ -52,10 +66,21 @@ public class Ladrillo extends Sprite implements Eliminable {
         }
         pelotita = new Rectangle(p.getX(), p.getY(), p.getWidth(), p.getHeight());
         if (pelotita.intersects(ladrillo)) {
-            p.setIncreY(-p.getIncreY());
+            if (pelotita.intersects(ladrilloLadoDcha) || pelotita.intersects(ladrilloLadoIzq)) {
+                p.setIncreX(-p.getIncreX());
+            } else {
+                p.setIncreY(-p.getIncreY());
+            }
+            if (numeroSkin == 7) {
+                golpes++;
+                if (golpes == 3) {
+                    return true;
+                } else {
+                    return false;
+                }
+            }
             return true;
         }
         return false;
-    }
-      
+    } 
 }
