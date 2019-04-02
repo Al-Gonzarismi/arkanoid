2d09f065825be3210f13d2aad09c6c10358f2c1d
1_web9_20 <1_web9_20@A08_PC01.JOVELLANOS.EDU>
2019-04-02 17:42:45 +0200

    meter habilidades nuevas y controlar cuanto tiempo duran cada una

--- src/figuras/LadrilloHabilidad.java
+++ src/figuras/LadrilloHabilidad.java
@@ -6,22 +6,31 @@
 package figuras;
 
 import figuras.base.Animable;
+import figuras.base.Dibujable;
+import figuras.base.Eliminable;
 import figuras.base.Sprite;
+import java.awt.Rectangle;
+import java.util.*;
 import logic.GameLogic;
 
 /**
  *
  * @author 1_web9_20
  */
-public class LadrilloHabilidad extends Sprite implements Animable {
+public class LadrilloHabilidad extends Sprite implements Animable, Eliminable {
+
     GameLogic logic;
+    Rectangle ladrilloHabilidad;
+    Rectangle nave;
+    List<Dibujable> l;
+
     public LadrilloHabilidad(GameLogic logic, int x, int y, int numeroSkin) {
-        super(new String[] {
+        super(new String[]{
             "assets/img/special_blue.png",
-            "assets/img/special_green.png",
             "assets/img/special_magenta.png",
-            "assets/img/special_orange.png",
-            "assets/img/special_red.png"
+            "assets/img/special_red.png",
+            "assets/img/special_orange.png", //dopar la nave
+            "assets/img/special_orange.png", // relentizar la nave
         });
         setX(x);
         setY(y);
@@ -29,11 +38,53 @@ public class LadrilloHabilidad extends Sprite implements Animable {
         setWidth(60);
         setHeight(22);
         this.logic = logic;
+        l = new ArrayList<>(logic.getListaObjetos());
+
     }
 
     @Override
     public void mover() {
-        setY(getY() - 5);
+        setY(getY() + 5);
     }
-    
+
+    public void habilidad(int i) {
+        Breakout b = null;
+        Pelota p = null;
+        for (Dibujable d : l) {
+            if (d instanceof Breakout) {
+                b = (Breakout) d;
+            }
+            if (d instanceof Pelota) {
+                p = (Pelota) d;
+            }
+        }
+        if (i == 0) {
+            b.setSkin(1);
+        } else if (i == 1) {
+            p.setIncreY(p.getIncreY() * 2);
+            p.setIncreX(p.getIncreX() * 2);
+
+        } else if (i == 2) {
+            logic.setVidas(logic.getVidas() + 1);
+        }
+    }
+
+    @Override
+    public boolean estaEliminado() {
+        Breakout b = null;
+        for (Dibujable d : l) {
+            if (d instanceof Breakout) {
+                b = (Breakout) d;
+                break;
+            }
+        }
+        nave = new Rectangle(b.getX(), b.getY(), b.getWidth(), b.getHeight());
+        ladrilloHabilidad = new Rectangle(getX(), getY(), getWidth(), getHeight());
+        if (nave.intersects(ladrilloHabilidad)) {
+            habilidad(getSelectedSkin());
+            return true;
+        }
+        return false;
+    }
+
 }
