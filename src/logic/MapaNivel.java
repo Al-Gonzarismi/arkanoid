b5fe7bdb6888b9053083c02ce37a642269cbcf6b
gonza <1_web9_20@iesjovellanos.org>
2019-04-03 11:41:18 +0200

    PRIMERA VERSION ESTABLE

--- src/logic/MapaNivel.java
+++ src/logic/MapaNivel.java
@@ -14,18 +14,104 @@ public class MapaNivel {
     // b=blue; c=cyan; g = green; m = magenta
     // o = orange, h = ladrillo duro
     public static final String[][] mapa = {
-        // Nivel 0
+        //Nivel 1
         {
-            "bbcc cbb",
-  
+            "cccccccc",
+            "cccccccc",
+            "cc      ",
+            "cc      ",
+            "cc      ",
+            "cc      ",
+            "cc  cccc",
+            "cc  cccc",
+            "cc    cc",
+            "cc    cc",
+            "cccccccc",
+            "cccccccc"
         },
-        // Nivel 1
+        // Nivel 2
         {
-            "bbccccbb",
-            "gghmmhgg",
-            "mmhoohmm",
-            "ryyyyyyr"
-        }
+            "        ",
+            "        ",
+            "        ",
+            "b c my r",
+            "b c my r",
+            "b c my r",
+            "b c my r",
+            "b c my r",
+            "b c my r",
+            "b c my r"
+        },
+        // Nivel 3
+        {
+            "        ",
+            "        ",
+            "        ",
+            "hmhmhmhm",
+            "rrrhmrrr",
+            "rrgmhgrr",
+            "rgghmggr",
+            "gggmhggg",
+            "ggyhmygg",
+            "gyymhyyg",
+            "yyyhmyyy"
+        },
+        // Nivel 4
+        {
+            "        ",
+            "        ",
+            "        ",
+            "   rr   ",
+            "  roor  ",
+            "  roor  ",
+            " royyor ",
+            " royyor ",
+            " royyor ",
+            "roybbyor",
+            "roybbyor",
+            "roybbyor",
+            "hhh  hhh"
+        },
+        // Nivel 5
+        {
+            "        ",
+            "        ",
+            "        ",
+            "  hbbh  ",
+            " hhbbhh ",
+            " hohhrh ",
+            " hohhrh ",
+            "hooorrrh",
+            "hooorrrh",
+            "hooorrrh",
+            "hooorrrh",
+            " hoorrh ",
+            " hoorrh ",
+            "  horh  ",
+            "  horh  ",
+            "   hh   "
+        },
+        // Nivel 6
+        {
+            "        ",
+            "        ",
+            "        ",
+            "        ",
+            "hhh  hhh",
+            "hhh  hhh",
+            " hchhch ",
+            " hcccch ",
+            "  hmmh  ",
+            "  hmmh  ",
+            "   mm   ",
+            "   mm   ",
+            "  hmmh  ",
+            "  hmmh  ",
+            " hcccch ",
+            " hchhch ",
+            "hhh  hhh",
+            "hhh  hhh",
+        }  
     };
 
 }
